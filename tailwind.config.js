9f24a6d90ac6776e8d266b5372f4dd56179eac41
Fandu0823 <fandudipoanderson82@gmail.com>
2024-12-09 23:42:06 +0700

    nyoba buat klik profil transisiton ilang timbul

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -12,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
             },
         },
     },
